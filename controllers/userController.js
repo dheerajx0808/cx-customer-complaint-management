4c06454b5165b3ed511fd632b0363fcb02aa31dd
dk <dheerajx0808@gmail.com>
2024-09-19 20:14:29 +0530

    feat : Implementing put /users endpoint to update the user details

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,12 +9,15 @@ const pool = new Pool({
 });
 
 
+
+
 exports.getUser = (req, res) => {
   const userId = req.params.userId;
   pool.query('SELECT * FROM complaint_management.customer_account WHERE username = $1', [userId], (error, results) => {
     if (error) {
       throw error;
     }
+    
     res.status(200).json(results.rows);
   });
 };
@@ -34,3 +37,47 @@ exports.postUser = (req, res) => {
     }
   );
 };
+
+
+exports.putUser =  (req, res) => {
+  const { firstname, lastname, username, email, country, locale, address } = req.body;
+  const { userId } = req.params;
+  try {
+    const result =  pool.query(
+      'UPDATE complaint_management.customer_account SET firstname = $1, lastname = $2, username = $3, email = $4, country = $5, locale = $6, address = $7 WHERE username = $8 RETURNING *',
+      [firstname, lastname, username, email, country, locale, address, userId]
+    );
+
+    console.log("result", result);
+   //res.status(200).json("Updated the record");
+
+    res.status(200).json(result.rows[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
+  }
+};
+
+// Update specific user fields
+exports.patchUser = (req, res) => {
+  const { userId } = req.params;
+  const fields = Object.keys(req.body);
+  const values = Object.values(req.body);
+
+
+  //pool.query('UPDATE customer_account SET firstname = $1, email = $2 WHERE id = $3', 
+          // ['kx', 'dkx@gmail.com', userId]);
+  
+  let query = 'UPDATE customer_account SET ';
+  query += fields.map((field, index) => `${field} = $${index + 1}`).join(', ');
+  query += ' WHERE id = $' + (fields.length + 1) + ' RETURNING *';
+  
+  try {
+    const result = pool.query(query, [...values, userId]);
+    res.status(200).json(result.rows[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
+  }
+};
+
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,5 +5,11 @@ const userController = require('../controllers/userController');
 
 router.get('/:userId', userController.getUser);
 router.post('/:userId', userController.postUser);
+router.put('/:userId', userController.putUser);
+router.patch('/:userId', userController.patchUser);
 
 module.exports = router;
+
+
+
+
